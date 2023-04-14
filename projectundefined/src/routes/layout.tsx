37fbee349999f34f43765a6d832fb171830e2afb
Wesley Schell <wesley.schell.sh@proton.me>
2023-04-14 11:39:44 -0500

    erase default values

--- projectundefined/src/routes/layout.tsx
+++ projectundefined/src/routes/layout.tsx
@@ -1,8 +1,6 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -13,15 +11,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 export default component$(() => {
   return (
     <div class="page">
-      <main>
-        <Header />
-        <Slot />
-      </main>
-      <div class="section dark">
-        <div class="container">
-          <Footer />
-        </div>
-      </div>
+     
     </div>
   );
 });
